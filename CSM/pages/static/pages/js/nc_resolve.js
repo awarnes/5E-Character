0839207173339641a8d795654f1832dce23d721a
Alex Warnes <warnes.alexander@gmail.com>
2017-03-24 20:55:13 -0700

    Further polish of character sheet.

--- CSM/pages/static/pages/js/nc_resolve.js
+++ CSM/pages/static/pages/js/nc_resolve.js
@@ -21,4 +21,63 @@ $(document).ready(function(){
         $('#delete_check').modal('toggle');
     });
 
+    $('.search_query').on('click', function (evt){
+
+        var $query = $(this).data('slug');
+
+        var re = /\s/g;
+
+        var $type = $(this).data('type').toLowerCase();
+
+        $type = $type.replace(re, '_');
+
+        $('#output').modal('toggle');
+
+        const $equipment = ['weapons', 'armors', 'items', 'tools'];
+
+        if ($type === 'spells') {
+            $.get('/api/v1/spells/spell/'+$query, function (data){
+                $('#name').text(data.name);
+                $('#name').data('type', $type);
+                $('#name').data('slug', $query);
+                $('#description').text(data.description);
+            })
+        }   else if ($equipment.indexOf($type) != -1) {
+            $.get('/api/v1/equipment/' + $type + '/' + $query, function (data){
+                $('#name').text(data.name);
+                $('#name').data('type', $type);
+                $('#name').data('slug', $query);
+                $('#description').text(data.description);
+            })
+        }  else {
+            $.get('/api/v1/rules/' + $type + '/' + $query, function (data){
+                $('#name').text(data.name);
+                $('#name').data('type', $type);
+                $('#name').data('slug', $query);
+                $('#description').text(data.description);
+            })
+        }
+
+
+    });
+
+    $('#open_details').on('click', function(evt){
+            var $type = $('#name').data('type');
+            var $slug = $('#name').data('slug')
+            window.location.href='/' + $type + '/details/' + $slug + '/'
+
+    });
+
+    $('.search_type').on('click', function (evt){
+
+        var $type_data = $(this).attr('data-type');
+
+        var queries = document.getElementsByClassName('search_query');
+
+        for (var i=0; i<queries.length; i++){
+            if (queries[i].dataset.search_type === $type_data) {
+                $(queries[i]).slideToggle();
+            }
+        }
+    });
 });
